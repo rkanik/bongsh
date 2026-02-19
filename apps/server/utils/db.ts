cc27f8c0132a75bb8e0fb93bf5128b12e810b2c5
RK Anik <30260735+rkanik@users.noreply.github.com>
2026-02-19 19:10:38 +0600

    static fi

--- apps/server/utils/db.ts
+++ apps/server/utils/db.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from "@/generated/prisma/client.ts";
 import { PrismaPg } from "@prisma/adapter-pg";
-import process from "node:process";
+import { DATABASE_URL } from "@/const.ts";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.SUPABASE_DATABASE_URL,
-});
-
+const adapter = new PrismaPg({ connectionString: DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
 
 export const db = prisma;
