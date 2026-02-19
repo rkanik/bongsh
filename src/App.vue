cf859a853088660a16c933e6f49a147224b4ee2c
RK Anik <30260735+rkanik@users.noreply.github.com>
2026-02-20 00:09:27 +0600

    Added shadcn

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
+import { Button } from '@/components/ui/button'
 
 const users = ref<any[]>([])
 const serverUrl = import.meta.env.VITE_SERVER_URL
@@ -18,6 +19,7 @@ onMounted(() => {
 
 <template>
   <div>
+    <Button>Click me</Button>
     <h1>Users</h1>
     <ul>
       <li v-for="user in users" :key="user.id">
