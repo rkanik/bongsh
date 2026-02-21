e2a8859765b947caf4dbc1f2cfd26eb525eb65f2
RK Anik <30260735+rkanik@users.noreply.github.com>
2026-02-22 03:31:17 +0600

    test

--- app/pages/app/settings/index.vue
+++ app/pages/app/settings/index.vue
@@ -12,6 +12,10 @@ definePageMeta({
   <div class="p-4">
     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ipsum id rem natus,
     voluptates quia, quasi, soluta earum doloribus quisquam optio dolorum architecto nisi eligendi
-    doloremque accusantium at aliquam. Excepturi.
+    doloremque accusantium at aliquam. Excepturi. Lorem ipsum dolor, sit amet consectetur
+    adipisicing elit. Amet earum minus rem maiores enim ipsum accusantium ab provident dolore
+    perspiciatis! Repellendus illum tempora odio commodi esse quasi iste maiores ad, ex voluptatibus
+    quisquam quaerat libero animi. Suscipit omnis autem quasi temporibus inventore, fugit
+    repellendus et. Ad voluptatibus quibusdam iusto perspiciatis.
   </div>
 </template>
